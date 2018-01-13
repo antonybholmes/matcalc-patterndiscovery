6e044b0d58d0fefcebb6a94d8f2778a5500cec45
Antony Holmes <antony.b.holmes@gmail.com>
2018-01-13 17:34:08 -0500

    Updates and bug fixes.

--- edu.columbia.rdf.matcalc.toolbox.patterndiscovery.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/patterndiscovery/PatternDiscoveryDialog.java
+++ edu.columbia.rdf.matcalc.toolbox.patterndiscovery.app/src/main/java/edu/columbia/rdf/matcalc/toolbox/patterndiscovery/PatternDiscoveryDialog.java
@@ -251,7 +251,7 @@ public class PatternDiscoveryDialog extends ModernDialogHelpWindow
    * Creates the ui.
    */
   private final void createUi() {
-    // this.getContentPane().add(new JLabel("Change " +
+    // this.getWindowContentPanel().add(new JLabel("Change " +
     // getProductDetails().getProductName() + " settings", JLabel.LEFT),
     // BorderLayout.PAGE_START);
 
